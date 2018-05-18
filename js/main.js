02ee8f80ba9eb3a54c673de50a33e530fab08af2
Caleb Rivers <cal1riv@gmail.com>
2018-05-18 11:17:34 +1000

    added game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
 console.log("Up and running");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+
+var cards = ["queen" , "queen" , "king" , "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[1];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	console.log("Nice!");
+	if (cardsInPlay[0] === cardsInPlay [1]) {
+		alert("you found a match ");
+	}
+	else {
+			alert ("you did not find a match");
+		}
+}
